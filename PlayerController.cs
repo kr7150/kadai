7e22d75ac7515e253901ce5220b6840358471a53
kr7150 <kitakitakita0608@gmail.com>
2025-07-17 15:49:38 +0900

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+   [SerializeField] int _playerScore;
+    public int hp;
+    [SerializeField] float _speed = 5;
+    [SerializeField] Text _text;
+    Rigidbody2D _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        Load();
+        _text.text = "スコア " + _playerScore.ToString();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * _speed;
+        _rb.velocity = move;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            Coin coinScript = collision.GetComponent<Coin>();
+            _playerScore += coinScript._score;
+           Destroy(collision.gameObject);
+            _text.text = "スコア " + _playerScore.ToString();
+            Save();
+        }
+    }
+
+    private void Load()
+    {
+        _playerScore = PlayerPrefs.GetInt("Score", 0);
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetInt("Score", _playerScore);
+    }
+}
